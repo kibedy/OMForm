9da89598a0c0661688dadb1be2db022f7f76a50a
kibedy <kibedy@gmail.com>
2018-11-06 23:20:07 +0100

    surface picturebox works can change listview1 order / todo: change id

--- OrthoMachine/View/ImageProcess.Designer.cs
+++ OrthoMachine/View/ImageProcess.Designer.cs
@@ -38,6 +38,12 @@ namespace OrthoMachine.View
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.listView2 = new System.Windows.Forms.ListView();
+            this.buttonPhotoDown = new System.Windows.Forms.Button();
+            this.buttonPhotoDel = new System.Windows.Forms.Button();
+            this.buttonPhotoUp = new System.Windows.Forms.Button();
+            this.buttonSurfaceDel = new System.Windows.Forms.Button();
+            this.buttonSurfaceUp = new System.Windows.Forms.Button();
+            this.buttonSurfaceDown = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
@@ -55,7 +61,7 @@ namespace OrthoMachine.View
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(4, 27);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(930, 473);
+            this.panel1.Size = new System.Drawing.Size(930, 383);
             this.panel1.TabIndex = 3;
             this.panel1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
             // 
@@ -68,7 +74,7 @@ namespace OrthoMachine.View
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.addMarker);
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.addMarkerPhoto);
             this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
@@ -77,7 +83,7 @@ namespace OrthoMachine.View
             // 
             this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(4, 431);
+            this.listView1.Location = new System.Drawing.Point(4, 428);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(392, 69);
             this.listView1.TabIndex = 4;
@@ -124,23 +130,107 @@ namespace OrthoMachine.View
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Visible = false;
+            this.pictureBox2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.addMarkerSurface);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
             // 
             // listView2
             // 
             this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.listView2.GridLines = true;
-            this.listView2.Location = new System.Drawing.Point(542, 431);
+            this.listView2.Location = new System.Drawing.Point(542, 428);
             this.listView2.Name = "listView2";
             this.listView2.Size = new System.Drawing.Size(392, 69);
             this.listView2.TabIndex = 7;
             this.listView2.UseCompatibleStateImageBehavior = false;
             this.listView2.Visible = false;
             // 
+            // buttonPhotoDown
+            // 
+            this.buttonPhotoDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPhotoDown.BackgroundImage = global::OrthoMachine.Properties.Resources.down_arrow;
+            this.buttonPhotoDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonPhotoDown.Location = new System.Drawing.Point(402, 451);
+            this.buttonPhotoDown.Name = "buttonPhotoDown";
+            this.buttonPhotoDown.Size = new System.Drawing.Size(24, 23);
+            this.buttonPhotoDown.TabIndex = 2;
+            this.buttonPhotoDown.UseVisualStyleBackColor = true;
+            this.buttonPhotoDown.Visible = false;
+            // 
+            // buttonPhotoDel
+            // 
+            this.buttonPhotoDel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPhotoDel.BackgroundImage = global::OrthoMachine.Properties.Resources.red_X;
+            this.buttonPhotoDel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonPhotoDel.Location = new System.Drawing.Point(402, 474);
+            this.buttonPhotoDel.Name = "buttonPhotoDel";
+            this.buttonPhotoDel.Size = new System.Drawing.Size(24, 23);
+            this.buttonPhotoDel.TabIndex = 8;
+            this.buttonPhotoDel.UseVisualStyleBackColor = true;
+            this.buttonPhotoDel.Visible = false;
+            // 
+            // buttonPhotoUp
+            // 
+            this.buttonPhotoUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPhotoUp.BackgroundImage = global::OrthoMachine.Properties.Resources.up_arrow;
+            this.buttonPhotoUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonPhotoUp.Location = new System.Drawing.Point(402, 428);
+            this.buttonPhotoUp.Name = "buttonPhotoUp";
+            this.buttonPhotoUp.Size = new System.Drawing.Size(24, 23);
+            this.buttonPhotoUp.TabIndex = 1;
+            this.buttonPhotoUp.UseVisualStyleBackColor = true;
+            this.buttonPhotoUp.Visible = false;
+            this.buttonPhotoUp.Click += new System.EventHandler(this.buttonPhotoUp_Click);
+            // 
+            // buttonSurfaceDel
+            // 
+            this.buttonSurfaceDel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSurfaceDel.BackgroundImage = global::OrthoMachine.Properties.Resources.red_X;
+            this.buttonSurfaceDel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonSurfaceDel.Location = new System.Drawing.Point(512, 474);
+            this.buttonSurfaceDel.Name = "buttonSurfaceDel";
+            this.buttonSurfaceDel.Size = new System.Drawing.Size(24, 23);
+            this.buttonSurfaceDel.TabIndex = 11;
+            this.buttonSurfaceDel.UseVisualStyleBackColor = true;
+            this.buttonSurfaceDel.Visible = false;
+            // 
+            // buttonSurfaceUp
+            // 
+            this.buttonSurfaceUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSurfaceUp.BackgroundImage = global::OrthoMachine.Properties.Resources.up_arrow;
+            this.buttonSurfaceUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonSurfaceUp.Location = new System.Drawing.Point(512, 428);
+            this.buttonSurfaceUp.Name = "buttonSurfaceUp";
+            this.buttonSurfaceUp.Size = new System.Drawing.Size(24, 23);
+            this.buttonSurfaceUp.TabIndex = 9;
+            this.buttonSurfaceUp.UseVisualStyleBackColor = true;
+            this.buttonSurfaceUp.Visible = false;
+            // 
+            // buttonSurfaceDown
+            // 
+            this.buttonSurfaceDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSurfaceDown.BackgroundImage = global::OrthoMachine.Properties.Resources.down_arrow;
+            this.buttonSurfaceDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonSurfaceDown.Location = new System.Drawing.Point(512, 451);
+            this.buttonSurfaceDown.Name = "buttonSurfaceDown";
+            this.buttonSurfaceDown.Size = new System.Drawing.Size(24, 23);
+            this.buttonSurfaceDown.TabIndex = 10;
+            this.buttonSurfaceDown.UseVisualStyleBackColor = true;
+            this.buttonSurfaceDown.Visible = false;
+            // 
             // ImageProcess
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(940, 503);
+            this.Controls.Add(this.buttonSurfaceDel);
+            this.Controls.Add(this.buttonSurfaceUp);
+            this.Controls.Add(this.buttonSurfaceDown);
+            this.Controls.Add(this.buttonPhotoDel);
+            this.Controls.Add(this.buttonPhotoUp);
+            this.Controls.Add(this.buttonPhotoDown);
             this.Controls.Add(this.listView2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.listView1);
@@ -173,5 +263,11 @@ namespace OrthoMachine.View
         private Panel panel2;
         private PictureBox pictureBox2;
         private ListView listView2;
+        private Button buttonPhotoUp;
+        private Button buttonPhotoDown;
+        private Button buttonPhotoDel;
+        private Button buttonSurfaceDel;
+        private Button buttonSurfaceUp;
+        private Button buttonSurfaceDown;
     }
 }
--- OrthoMachine/View/ImageProcess.cs
+++ OrthoMachine/View/ImageProcess.cs
@@ -23,6 +23,9 @@ namespace OrthoMachine.View
         bool _tracking = false;
         List<Marker> markers;
         Form1 form1;
+        bool EnablePickpoints;
+        //bool EnablePictureBox1Functions;
+        Image<Gray, ushort> surface;
 
 
         public ImageProcess(Form1 form1, string filename, List<Marker> markers)
@@ -34,12 +37,14 @@ namespace OrthoMachine.View
             Image<Bgr, byte> photo = new Image<Bgr, byte>(form1.SavePath + "\\photos\\"+filename);
             this.pictureBox1.Image = photo.ToBitmap();
             this.pictureBox1.Cursor = Cursors.Hand;
+            
+            //EnablePictureBox1Functions = true;
 
             ImageWidth = pictureBox1.Image.Width;
             ImageHeight = pictureBox1.Image.Height;
             this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             this.pictureBox1.Size = new Size(ImageWidth, ImageHeight);
-
+            EnablePickpoints = false;
             /*PictureBox pictureBoxOverlay = new PictureBox();
             pictureBoxOverlay.BackColor = Color.Transparent;
             pictureBoxOverlay.Location = new Point(0, 0);
@@ -59,23 +64,22 @@ namespace OrthoMachine.View
 
 
 
-
-
+        #region PictureBox_1_Events
         private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
         {
             const float scale_per_delta = 0.1f / 120;
             ImageScale += e.Delta * scale_per_delta;
-            if (ImageScale < 0) ImageScale = 0;
-            this.pictureBox1.Size = new Size(
-                (int)(ImageWidth * ImageScale),
-                (int)(ImageHeight * ImageScale));
-            if ((pictureBox1.Image.Width > this.ClientSize.Width || pictureBox1.Image.Height > this.ClientSize.Height))
-            {
-                panel1.AutoScrollPosition = new Point(-panel1.AutoScrollPosition.X + (e.X),-panel1.AutoScrollPosition.Y + (e.Y));
-                ;
-            }
-
+            
+            
+                if (ImageScale < 0) ImageScale = 0;
+                this.pictureBox1.Size = new Size((int)(ImageWidth * ImageScale), (int)(ImageHeight * ImageScale));
+                if ((pictureBox1.Image.Width > this.ClientSize.Width || pictureBox1.Image.Height > this.ClientSize.Height))
+                {
+                    panel1.AutoScrollPosition = new Point(-panel1.AutoScrollPosition.X + (e.X), -panel1.AutoScrollPosition.Y + (e.Y));
+                }
+            
         }
+
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
@@ -83,25 +87,57 @@ namespace OrthoMachine.View
             {
                 _mousePt = e.Location;
                 _tracking = true;
-                ;
             }
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (_tracking &&
-             (pictureBox1.Image.Width > this.ClientSize.Width ||
-             pictureBox1.Image.Height > this.ClientSize.Height))
+            if (_tracking  && (pictureBox1.Image.Width > this.ClientSize.Width || pictureBox1.Image.Height > this.ClientSize.Height))
             {
-                panel1.AutoScrollPosition = new Point(-panel1.AutoScrollPosition.X + (_mousePt.X - e.X),
-                 -panel1.AutoScrollPosition.Y + (_mousePt.Y - e.Y));
+                panel1.AutoScrollPosition = new Point(-panel1.AutoScrollPosition.X + (_mousePt.X - e.X), -panel1.AutoScrollPosition.Y + (_mousePt.Y - e.Y));
             }
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
+            
+                _tracking = false;
+            
+
+        }
+
+
+        #endregion
+
+        # region PictureBox_2_Events
+        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                _mousePt = e.Location;
+                _tracking = true;
+            }
+        }
+
+
+        private void pictureBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_tracking &&
+             (pictureBox2.Image.Width > this.ClientSize.Width ||
+             pictureBox2.Image.Height > this.ClientSize.Height))
+            {
+                panel2.AutoScrollPosition = new Point(-panel2.AutoScrollPosition.X + (_mousePt.X - e.X),
+                 -panel2.AutoScrollPosition.Y + (_mousePt.Y - e.Y));
+            }
+        }
+
+
+        private void pictureBox2_MouseUp(object sender, MouseEventArgs e)
+        {
             _tracking = false;
         }
+        #endregion
+
 
         private void panel1_MouseWheel(object sender, MouseEventArgs e)
         {
@@ -109,7 +145,8 @@ namespace OrthoMachine.View
         }
 
         private void orientateToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        {            
+            EnablePickpoints = true;
             panel1.Height -= 80;
             panel1.Size = new Size(this.Width / 2-2, panel1.Height);
             //panel2.Size = new Size(this.Width / 2, panel1.Height);
@@ -118,12 +155,19 @@ namespace OrthoMachine.View
             panel2.Location= new Point( panel1.Location.X+panel1.Width+2, panel1.Location.Y) ;
             panel2.Width = panel1.Width-4;
            
-            Image<Gray, ushort> surf = new Image<Gray, ushort>(form1.SavePath + "\\" + "surface.png");
-            this.pictureBox2.Image = surf.ToBitmap();
+            surface = new Image<Gray, ushort>(form1.SavePath + "\\" + "surface.png");
+            this.pictureBox2.Image = surface.ToBitmap();
             this.pictureBox2.Cursor = Cursors.Hand;
             this.pictureBox2.Visible = true;
             this.panel2.Visible = true;
             this.SizeChanged += new System.EventHandler(this.ImageProcess_SizeChanged);
+            this.buttonPhotoUp.Visible = true;
+            this.buttonPhotoDown.Visible = true;
+            this.buttonPhotoDel.Visible = true;
+            this.buttonSurfaceDel.Visible = true;
+            this.buttonSurfaceUp.Visible = true;
+            this.buttonSurfaceDown.Visible = true;
+
         }
 
         private void ImageProcess_SizeChanged(object sender, EventArgs e)
@@ -135,27 +179,53 @@ namespace OrthoMachine.View
             panel2.Location = new Point(panel1.Location.X + panel1.Width + 2, panel1.Location.Y);
         }
 
-        private void addMarker(object sender, MouseEventArgs e)
-        {
-            //string tablaformazas = "{0,-10}{1,-20}";
-            Point pp = e.Location;
-            markers.Add(new Marker(pp.X / ImageScale, pp.Y / ImageScale, filename));
-            //string listaelem = (pp.X / ImageScale).ToString() + ", " + (pp.Y / ImageScale).ToString();
-
-           
-            ;
-
-            var item = new ListViewItem(new[] {(markers.Count-1).ToString() ,(pp.X / ImageScale).ToString(), (pp.Y / ImageScale).ToString() });
-
-            this.listView1.Items.Add(item);
-
-
-            ;
-
+        private void addMarkerPhoto(object sender, MouseEventArgs e)
+        {   if (EnablePickpoints)
+            {
+                Point pp = e.Location;
+                //markers.Add(new Marker(pp.X / ImageScale, pp.Y / ImageScale, filename));
+                var item = new ListViewItem(new[] { listView1.Items.Count.ToString(), (pp.X / ImageScale).ToString("0.00"), (pp.Y / ImageScale).ToString("0.00") });
+                this.listView1.Items.Add(item);
+            }
         }
 
-      
-        
+        private void buttonPhotoUp_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count>2)
+            {
+                /*int index = listView1.SelectedIndices[0];
+                index--;
+                this.listView1.Items[index].Selected = true;
+                listView1.Refresh();*/
+                ListViewItem item = listView1.SelectedItems[0];
+                int index = listView1.SelectedItems[0].Index;
+                ListViewItem itemUpper = listView1.Items[index-1];
+                listView1.Items.RemoveAt(item.Index - 1);
+                listView1.Items.RemoveAt(item.Index);
+                listView1.Items.Insert(index - 1, item);
+                listView1.Items.Insert(index,itemUpper);                
+                //listView1.Items[item.Index-1] = item;
+                listView1.Refresh();
+                //TODO change ID number;
+            }
+        }
+
+        private void addMarkerSurface(object sender, MouseEventArgs e)
+        {
+            if (EnablePickpoints)
+            {
+                Point pp = e.Location;
+                //markers.Add(new Marker(pp.X / ImageScale, pp.Y / ImageScale, filename));
+                int xx = (int)(pp.X / ImageScale);
+                int yy = (int)(pp.Y / ImageScale);
+                var item = new ListViewItem(new[] { (listView2.Items.Count.ToString()).ToString(), xx.ToString("0"),(surface.Data[yy,xx,0]).ToString()  ,yy.ToString("0") });
+                this.listView2.Items.Add(item);
+            }
+            
+            
+        }
+
+
 
 
         /* private void pictureBox1_Click(object sender, EventArgs e)
